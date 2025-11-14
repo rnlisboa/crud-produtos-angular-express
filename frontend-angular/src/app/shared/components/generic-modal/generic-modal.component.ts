51cd239b63e128bc5902d69caca3d8db7fd602a6
rnlisboa <dev.renanlisboa@gmail.com>
2025-11-14 11:36:28 -0300

    cria listagem de itens do pedido

--- frontend-angular/src/app/shared/components/generic-modal/generic-modal.component.ts
+++ frontend-angular/src/app/shared/components/generic-modal/generic-modal.component.ts
@@ -1,20 +1,94 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import PedidoItemEntity from '../../../interfaces/domain/pedido-item.entity';
-import { PoModalComponent, PoModalModule } from '@po-ui/ng-components';
+import {
+  PoButtonModule,
+  PoFieldModule,
+  PoInfoModule,
+  PoListViewModule,
+  PoModalComponent,
+  PoModalModule,
+  PoNotificationService,
+} from '@po-ui/ng-components';
 import { PedidoItemResponse } from '../../../interfaces/response/pedido-response';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { PedidoService } from '../../../services/pedidos/pedido.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-generic-modal',
-  imports: [PoModalModule],
+  imports: [
+    PoModalModule,
+    PoListViewModule,
+    PoInfoModule,
+    CurrencyPipe,
+    PoButtonModule,
+    PoFieldModule,
+    FormsModule,
+  ],
   templateUrl: './generic-modal.component.html',
   styleUrl: './generic-modal.component.less',
 })
 export class GenericModalComponent {
+  poNotification = inject(PoNotificationService);
   @ViewChild(PoModalComponent, { static: true }) poModal!: PoModalComponent;
   elementos!: PedidoItemResponse[];
+
+  @Output() itensAtualizados = new EventEmitter<void>();
+
+  editingItemId: string | null = null;
+  novaQuantidade: number | null = null;
+
+  constructor(private pedidoService: PedidoService) {}
+
+  iniciarEdicao(item: PedidoItemResponse) {
+    this.editingItemId = item.id;
+    this.novaQuantidade = item.quantidade;
+  }
+
+  salvarQuantidade(item: PedidoItemResponse) {
+    if (this.novaQuantidade == null) return;
+
+    this.pedidoService.updateItem(item.id, this.novaQuantidade).subscribe({
+      next: (res) => {
+        this.elementos = this.elementos.map((p) =>
+          p.id === item.id ? { ...p, quantidade: this.novaQuantidade! } : p
+        );
+
+        this.poNotification.success(res.message);
+        this.editingItemId = null;
+        this.novaQuantidade = null;
+
+        this.emitirAtualizacao();
+      },
+    });
+  }
+
+  deletarItemPedido(id: string) {
+    this.pedidoService.deleteItem(id).subscribe({
+      next: (res) => {
+        this.poNotification.success(res.message);
+        this.elementos = this.elementos.filter((item) => item.id !== id);
+        this.emitirAtualizacao();
+      },
+      error: (res) => {
+        this.poNotification.error(res);
+      },
+    });
+  }
 
   openModal(elementos: PedidoItemResponse[]) {
     this.elementos = elementos;
     this.poModal.open();
+  }
+
+  emitirAtualizacao() {
+    this.itensAtualizados.emit();
   }
 }
